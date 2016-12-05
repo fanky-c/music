55b40ce663b4317503522f1b8569f566dc064e9d
zhaochao <396850524@qq.com>
2016-12-05 17:47:43 +0800

    add

--- src/js/lib/musicVisualizer.js
+++ src/js/lib/musicVisualizer.js
@@ -79,49 +79,50 @@ const getArr = (ctx) => {
 /**
  * [Render 渲染]
  */
-const Render = (ctx,type) => {	
+const Render = (ctx,type,musicArray) => {	
 	var o = null;
-	var RenderCanvasType = type;		
-	var w = Math.round(WIDTH / SIZE);
-	var cgap = Math.round(w * 0.3);
-	var cw = w - cgap;
-	ctx.fillStyle = ARR.linearGradient;
-	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	var RenderCanvasType = type;
+	return function() {
+		var w = Math.round(WIDTH / SIZE);
+		var cgap = Math.round(w * 0.3);
+		var cw = w - cgap;
+		ctx.fillStyle = ARR.linearGradient;
+		ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-	for(var i = 0; i < SIZE; i++){		
-		o = ARR[i];
-		if(RenderCanvasType == 'Dot'){
-			var x = o.x,
-			y = o.y,
-			//r = Math.round((this[i]/2+18)*(HEIGHT > WIDTH ? WIDTH : HEIGHT)/(isMobile ? 500 : 800));
-			r = Math.round((i/2+18)*(HEIGHT > WIDTH ? WIDTH : HEIGHT)/(isMobile ? 500 : 800));
-			o.x += o.dx;
-			o.x > (WIDTH + r) && (o.x = - r);
 
-			//开始路径，绘画圆
-			ctx.beginPath();
-			ctx.arc(x, y, r, 0, Math.PI * 2, true);
-	    	var gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
-		    gradient.addColorStop(0, "rgb(255,255,255)");
-		    gradient.addColorStop(1, o.color);
-		    ctx.fillStyle = gradient;
-		    ctx.fill();		    
-		}
-		if(RenderCanvasType == 'Column'){
-			//var h = this[i] / 280 * HEIGHT;
-			var h = i / 280 * HEIGHT;
-			ARR[i].cheight > cw && (ARR[i].cheight = cw);
-			if(--ARR[i].cap < ARR[i].cheight){
-				ARR[i].cap = ARR[i].cheight;
-			};
-			if(h > 0 && (ARR[i].cap < h + 40)){
-				ARR[i].cap = h + 40 > HEIGHT ? HEIGHT : h + 40;
+		for (var i = 0; i < SIZE; i++) {
+			o = ARR[i];
+			if (RenderCanvasType == 'Dot') {
+				var x = o.x,
+					y = o.y,
+					r = Math.round((musicArray[i]/2+18)*(HEIGHT > WIDTH ? WIDTH : HEIGHT)/(isMobile ? 500 : 800));
+				o.x += o.dx;
+				o.x > (WIDTH + r) && (o.x = -r);
+
+				//开始路径，绘画圆
+				ctx.beginPath();
+				ctx.arc(x, y, r, 0, Math.PI * 2, true);
+				var gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
+				gradient.addColorStop(0, "rgb(255,255,255)");
+				gradient.addColorStop(1, o.color);
+				ctx.fillStyle = gradient;
+				ctx.fill();
 			}
-			ctx.fillRect(w * i, HEIGHT - ARR[i].cap, cw, ARR[i].cheight);			
-			ctx.fillRect(w * i, HEIGHT - h, cw, h);
+			if (RenderCanvasType == 'Column') {
+				var h = musicArray[i] / 280 * HEIGHT;
+				ARR[i].cheight > cw && (ARR[i].cheight = cw);
+				if (--ARR[i].cap < ARR[i].cheight) {
+					ARR[i].cap = ARR[i].cheight;
+				};
+				if (h > 0 && (ARR[i].cap < h + 40)) {
+					ARR[i].cap = h + 40 > HEIGHT ? HEIGHT : h + 40;
+				}
+				ctx.fillRect(w * i, HEIGHT - ARR[i].cap, cw, ARR[i].cheight);
+				ctx.fillRect(w * i, HEIGHT - h, cw, h);
+			}
+
 		}
-		
-	}	
+	}
 }
 
 //初始化heigth，width以及canvas的宽高
@@ -131,11 +132,11 @@ const init = () => {
 	canvasDot.height = canvasColumn.height = HEIGHT;
 	canvasDot.width = canvasColumn.width = WIDTH;
 	
-	getArr(ctxColumn);
-	Render(ctxColumn,'Column');
+	 getArr(ctxColumn);
+	// Render(ctxColumn,'Column');
 
-	getArr(ctxDot);
-	Render(ctxDot,'Dot');
+	 getArr(ctxDot);
+	// Render(ctxDot,'Dot');
 }
 
 /**
@@ -146,14 +147,6 @@ export const renderInit = () => {
      init();
      window.onresize = init;	  
 }
-
-
-
-// //Android和苹果设备则设置音乐片段为16
-// var SIZE = 32; //音乐片段数
-// isMobile && (SIZE = 16);
-
-
 
 
 /**
@@ -168,13 +161,11 @@ export let Visualizer = function(urlList,callBack){
 	 this.callBack = callBack;
 	 this.bufferList = new Array();
 	 this.loadCount = 0;
+	 this.play();
 }
 
 Visualizer.prototype = {
 	  constructor: Visualizer,
-	  init: function(){
-
-	  },
 	  load: function(url,index){
          var that = this;
          var context = that.context;
@@ -219,32 +210,52 @@ Visualizer.prototype = {
 
  var visualizer  = new Visualizer(
        ['http://localhost:9998/file/Sam Tsui - Sugar.mp3'],
-       function(bufferList,context){
-	         var requestAnimationFrame = window.requestAnimationFrame || 
-								window.webkitRequestAnimationFrame || 
-								window.oRequestAnimationFrame || 
-								window.mzRequestAnimationFrame;           	
+       function(bufferList,context){        	
 			 var source = context.createBufferSource();
 			 var analyser = context.createAnalyser();
-			 var musicArray = [];
-	         
-	         console.log(analyser)                  
+			 var gainNode = context.createGain();
+			 
+             //表示context中所有音频（节点）的最终目标节点，一般是音频渲染设备，比如扬声器。
+			 source.connect(context.destination); 
+	                        
              
-             //播放
-	             source.buffer = bufferList[0];
-	             source.connect(context.destination);
-	             source.start(0);
+             //播放和停止
+	             source.buffer = bufferList[0];  //第一首歌资源	            
+		         source.start ? source.start(0) : source.noteOn(0);  //播放
+                 //source.stop ? source.stop(0) : source.noteOff(0); //停止
+	             
 
 	        //分析音频源
-	         source.connect(analyser);
-	         musicArray = new Uint8Array(analyser.frequencyBinCount);
-			 function v() {
-				analyser.getByteFrequencyData(musicArray);
-				requestAnimationFrame(v);
-			 }
+	             //console.log('音频源',analyser)  
+		         source.connect(analyser);
+		         analyser.fftSize = SIZE * 2;
+		         var musicArray = new Uint8Array(analyser.frequencyBinCount);
+		         canvasAnimation(analyser,musicArray)
+		         console.log(musicArray)
 
-			requestAnimationFrame(v);
-
+	       //控制音量
+	           //console.log('音量',gainNode)
+		       source.connect(gainNode);
+		       gainNode.gain.value = 0.1;		
        }
- 	);	 
- visualizer.play();	
+  );	
+
+
+
+
+         function canvasAnimation(analyser,musicArray) {
+         	var requestAnimationFrame =
+         		window.requestAnimationFrame ||
+         		window.webkitRequestAnimationFrame ||
+         		window.oRequestAnimationFrame ||
+         		window.mzRequestAnimationFrame;
+
+         	function v() {
+         		analyser.getByteFrequencyData(musicArray);
+         		requestAnimationFrame(v);
+         		Render(ctxDot, 'Dot',musicArray)();
+         		Render(ctxColumn, 'Column',musicArray)();
+         	}
+
+         	requestAnimationFrame(v);
+         }
